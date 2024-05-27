553ca28cb823891b5c1631eefd40cd2cd51c58eb
Mathis Telschow <mathistelschow@gmail.com>
2024-05-27 19:02:52 +0200

    Add basic packages and configuration to project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import add from './add/add';
+const result: number = add(2, 4);
+
+console.log(result);
+
+export default result;
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,7 @@
+import result from '../index';
+
+describe('Suite info', () => {
+  it('expect result to equal 6', () => {
+    expect(result).toEqual(6);
+  });
+});
