32b2b541725e2007b496050db7a6f795edf55b08
Mathis Telschow <mathistelschow@gmail.com>
2024-06-06 20:54:11 +0200

    Remove temp files

--- src/tests/add/addSpec.ts
+++
@@ -1,9 +0,0 @@
-import add from '../../add/add';
-
-describe('Add function suite', () => {
-  describe('function testing', () => {
-    it('expect add(2, 5) to equal 7', () => {
-      expect(add(2, 5)).toEqual(7);
-    });
-  });
-});
