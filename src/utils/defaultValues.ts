7f347864f5f1dcd6e6087cd27936f806a6478350
Mathis Telschow <mathistelschow@gmail.com>
2024-06-08 13:49:36 +0200

    Add default value file

---
+++ src/utils/defaultValues.ts
@@ -0,0 +1,11 @@
+const DEFAULT_FILENAME: string = '';
+const DEFAULT_WIDTH: number = 200;
+const DEFAULT_HEIGHT: number = 200;
+const DEFAULT_FORMAT: string = 'jpg';
+
+export default {
+    DEFAULT_FILENAME,
+    DEFAULT_WIDTH,
+    DEFAULT_HEIGHT,
+    DEFAULT_FORMAT,
+}
