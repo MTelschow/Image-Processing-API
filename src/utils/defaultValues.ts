3405425c2c683a5e8f53b2b69927b322de51b6ff
Mathis Telschow <mathistelschow@gmail.com>
2024-06-08 15:02:26 +0200

    Add  more images

--- src/utils/defaultValues.ts
+++ src/utils/defaultValues.ts
@@ -1,8 +1,10 @@
+import { Formats } from 'resize-img';
+
 // Just a file to centrally store constants
 const DEFAULT_FILENAME: string = '';
 const DEFAULT_WIDTH: number = 200;
 const DEFAULT_HEIGHT: number = 200;
-const DEFAULT_FORMAT: string = 'jpg';
+const DEFAULT_FORMAT: Formats = 'jpg' as Formats;
 
 export default {
   DEFAULT_FILENAME,
