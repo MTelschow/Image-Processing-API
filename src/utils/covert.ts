973498ef1311ea6221e9bd9927ba0cb07d203bb7
Mathis Telschow <mathistelschow@gmail.com>
2024-06-08 14:32:49 +0200

    Add conversion.ts file to convert images

--- src/utils/covert.ts
+++ src/utils/covert.ts
@@ -2,49 +2,86 @@ import fs from 'fs';
 import resizeImg, { Formats } from 'resize-img';
 import fileSystemUtils from './fileSystemUtils';
 
+interface ResizeImgArgs {
+  width?: number;
+  height?: number;
+  format: Formats;
+}
+
+// Destructor fileSystemUtils functions
 const { fullPictureExists, thumbExists, getFullPicturePath, getThumbPath } =
   fileSystemUtils;
 
+// Method to handle conversion with
 const resizeTargetImage = async (
   filename: string,
-  width: number | undefined,
-  height: number | undefined,
-  format: string = 'jpg',
+  width: number,
+  height: number,
+  format: string,
 ): Promise<string> => {
-  // Create relevant paths
-  const resizedImagePath = getThumbPath(filename, width, height, format);
-
   // Handle file not findable
   if (!fullPictureExists(filename, format)) return '';
+
+  // If no height or width specified return full image
+  if (Number.isNaN(width) && Number.isNaN(height)) {
+    return getFullPicturePath(filename, format);
+  }
+
+  // Create target path
+  const resizedImagePath = getThumbPath(filename, width, height, format);
 
   // Skip process if file has already been created
   if (thumbExists(filename, width, height, format)) return resizedImagePath;
 
+  // Resize image
   const resizedImage = await convertImage(filename, width, height, format);
 
+  // Handle conversion error
+  if (resizedImage == undefined) return '';
 
+  // Write file to fs
   fs.writeFileSync(resizedImagePath, resizedImage);
 
-
-  
+  // Return file path
   return thumbExists(filename, width, height, format) ? resizedImagePath : '';
 };
 
+// Function to do conversion step
 const convertImage = async (
   filename: string,
   width: number,
   height: number,
-  format: string = 'jpg',
-): Promise<Buffer> => {
+  format: string,
+): Promise<Buffer | undefined> => {
+  // Get filepath
   const fullPicturePath = getFullPicturePath(filename, format);
 
-  const resizedImage = await resizeImg(fs.readFileSync(fullPicturePath), {
-    width: Number(width),
-    height: Number(height),
+  // Create args variable for resizeImg()
+  const resizeImgArgs: ResizeImgArgs = {
     format: format as Formats,
-  });
+  };
 
-  return resizedImage;
+  // Set width/height if not Nan
+  if (!Number.isNaN(width)) {
+    resizeImgArgs.width = width;
+  }
+  if (!Number.isNaN(height)) {
+    resizeImgArgs.height = height;
+  }
+  // Try Conversion
+  try {
+    console.log(`resizeImg(fs.readFileSync(${fullPicturePath}, ${resizeImgArgs});`)
+    const resizedImage = await resizeImg(
+      fs.readFileSync(fullPicturePath),
+      resizeImgArgs,
+    );
+
+    return resizedImage;
+  } catch (error) {
+    // Handle fail in conversion step
+    return undefined;
+  }
+
 };
 
 export default resizeTargetImage;
