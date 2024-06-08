7c0fd7061fe7ae3ca52af92d607220f2520907ce
Mathis Telschow <mathistelschow@gmail.com>
2024-06-08 16:21:29 +0200

    Add tests for default values

---
+++ src/tests/utils/defaultValuesSpec.test.ts
@@ -0,0 +1,22 @@
+import defaultValues from '../../utils/defaultValues';
+
+const { DEFAULT_FILENAME, DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_FORMAT } =
+  defaultValues;
+
+describe('Default Values suite', () => {
+  it('default filename should be a string', () => {
+    expect(DEFAULT_FILENAME).toBeInstanceOf(String);
+  });
+
+  it('default width should be a number', () => {
+    expect(DEFAULT_WIDTH).toBeInstanceOf(Number);
+  });
+
+  it('default height should be a number', () => {
+    expect(DEFAULT_HEIGHT).toBeInstanceOf(Number);
+  });
+
+  it('default format should be a valid, supported format', () => {
+    expect(DEFAULT_FORMAT).toMatch(/^(png|jpg|bmp)$/);
+  });
+});
