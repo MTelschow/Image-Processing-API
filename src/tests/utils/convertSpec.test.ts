59c476b0a88d742d870d268b740f1ee532b029dc
Mathis Telschow <mathistelschow@gmail.com>
2024-06-08 22:40:21 +0200

    Add test for convert image

---
+++ src/tests/utils/convertSpec.test.ts
@@ -0,0 +1,51 @@
+import { resizeTargetImage, convertImage } from '../../utils/covert';
+import {
+  getFullImagePath,
+  getThumbPath,
+  thumbExists,
+} from '../../utils/fileSystemUtils';
+import fs from 'fs';
+
+describe('convertImage suite', () => {
+  describe('resizeTargetImage', () => {
+    it("should return '' if image doesn't exist", async () => {
+      const output = await resizeTargetImage('does-not-exist', 200, 200, 'jpg');
+      expect(output).toBe('');
+    });
+
+    it('should return full image path if width and height NaN', async () => {
+      const filename = 'tiger';
+      const format = 'jpg';
+      const output = await resizeTargetImage(filename, NaN, NaN, format);
+      expect(output).toBe(getFullImagePath(filename, format));
+    });
+    it('should return correct thumb image path', async () => {
+      const filename = 'tiger';
+      const format = 'jpg';
+      const width = 300;
+      const height = 200;
+      const output = await resizeTargetImage(filename, width, height, format);
+      expect(output).toBe(getThumbPath(filename, width, height, format));
+    });
+    it("should return '' if conversion fails", async () => {
+      const output = await resizeTargetImage(
+        'does-not-exist',
+        -200,
+        -200,
+        'jpg',
+      );
+      expect(output).toBe('');
+    });
+  });
+  describe('convertImage', () => {
+    it('should convert image correctly', async () => {
+      const filename = 'tiger';
+      const format = 'jpg';
+      const width = 300;
+      const height = 200;
+      const output = await convertImage(filename, width, height, format);
+
+      expect(output).toBeInstanceOf(Buffer);
+    });
+  });
+});
